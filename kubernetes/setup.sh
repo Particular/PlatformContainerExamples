a6504a2c34e7b0dc386443381878d1d77c323f62
Bob Langley <bobiii@thelangleys.org>
2024-09-09 10:04:02 -0700

    Update ingress for basic auth

--- kubernetes/setup.sh
+++ kubernetes/setup.sh
@@ -17,12 +17,19 @@ kubectl wait --for=jsonpath='{.status.phase}'=Running pod/servicebus-server-0
 ## Load the transport configuration values into a configmap
 kubectl create configmap transport-config --from-env-file=./transport-config.env
 
-## Install the platform components
+## Apply the platform components
 kubectl apply -f ./servicecontrol-monitoring.deployment.yaml
 kubectl apply -f ./servicecontrol-audit.deployment.yaml
 kubectl apply -f ./servicecontrol-error.deployment.yaml
 kubectl apply -f ./servicepulse.deployment.yaml
 
-## Wait for the ingress to be ready and apply our config
+## Create a basic auth password file and import it as a secret
+htpasswd -c .htpasswd foo bar
+kubectl create secret generic basic-auth --from-file=./.htpasswd
+rm -rf .htpasswd
+
+## Wait for the ingress controller to be ready
 kubectl wait --namespace ingress-nginx --for=condition=ready pod --selector=app.kubernetes.io/component=controller --timeout=120s
+
+## Apply the ingress
 kubectl apply -f ./ingress.yaml
